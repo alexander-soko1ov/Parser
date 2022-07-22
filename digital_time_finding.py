101be0a75143fdab4745f6c38aac285f38ce39fa
alexander-soko1ov <alex.sokolov@internet.ru>
2022-07-22 23:12:21 +0300

    функция работает с датами в цифровом формате 'в 12:01, к 18:45', выводит часы, минуты, маску

--- digital_time_finding.py
+++ digital_time_finding.py
@@ -3,7 +3,7 @@ import random
 import datetime
 from termcolor import colored, cprint
 
-string = 'к 12:01'
+string = 'к 12:16'
 string_split = string.split()
 print(string_split)
 
@@ -12,9 +12,15 @@ def digital_time_finding(string_split, hour_k=0, minute_k=0, mask=0):
     """функция работает с датами в цифровом формате 'в 12:01, к 18:45',
      выводит часы, минуты, маску """
     datetime_today = datetime.datetime.today()
+    hour = datetime_today.hour
+    minute = datetime_today.minute
+
     mask_str_time = None
+    result = None
+
     for index_time, element_time in enumerate(string_split):
         if (':' in element_time) and (element_time.replace(':', '').isdigit()) and (len(element_time) in [4, 5]):
+
             word_before_element = string_split[index_time - 1]
             if len(element_time) == 4:
                 hour = element_time[:-3]
@@ -22,20 +28,19 @@ def digital_time_finding(string_split, hour_k=0, minute_k=0, mask=0):
             elif len(element_time) == 5:
                 hour = element_time[:-3]
                 minute = element_time[3:]
-            if word_before_element in ['в', 'к']:
+            if word_before_element in ['в', 'к', 'на']:
                 mask_str_time = word_before_element + ' ' + element_time
             else:
                 mask_str_time = element_time
-        else:
-            hour = datetime_today.hour
-            minute = datetime_today.minute
-        if hour_k == 1:
-            result = hour
-        elif minute_k == 1:
-            result = str(minute).lstrip('0')
 
-        if mask == 1:
-            result = mask_str_time
+
+    if hour_k == 1:
+        result = hour
+    elif minute_k == 1:
+        result = str(minute).lstrip('0')
+
+    if mask == 1:
+        result = mask_str_time
 
     return result
 
